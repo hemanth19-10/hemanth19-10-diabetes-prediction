5b1305c33885ca7388201ea310bf332bf2a3670e
unknown <you@example.com>
2026-05-30 12:02:35 +0530

    intial ml project

---
+++ test_api.py
@@ -0,0 +1,24 @@
+import requests
+
+# fastapi endpoints url
+
+API_URL="http://127.0.0.1:8000/predict"
+
+payload={
+  "Pregnancies": 2,
+  "Glucose": 140,
+  "BloodPressure": 100,
+  "SkinThickness": 30,
+  "Insulin": 80,
+  "BMI": 25,
+  "DiabetesPedigreeFunction": 0.355,
+  "Age": 28
+
+}
+
+#send POST request
+
+response=requests.post(API_URL,json=payload)
+
+print("status code:",response.status_code)
+print("Response JSON",response.json())
